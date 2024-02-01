26d37febd35ac44f4185af7298dc47963d12a1d7
Grzegorz Dominiczak <dominiczaq@gmail.com>
2024-02-01 11:29:19 +0100

    Bump current km

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App: Component = () => {
   const targetKm = 30_000;
   // TODO dynamic
   const [currentKm] = createSignal([
+    { updatedAt: new Date("2024-02-01T11:28"), value: 12_695 },
     { updatedAt: new Date("2024-01-31T12:09"), value: 12_604 },
     { updatedAt: new Date("2024-01-30T13:21"), value: 12_503 },
   ]);
